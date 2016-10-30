8834824063787866433ff97baa74c45f9075ba59
asksven <sven.knispel@gmail.com>
2016-10-30 12:07:49 +0100

    Handle case where the request has a body as a string as well as if it is a javascript object

--- SlackBot/index.js
+++ SlackBot/index.js
@@ -49,18 +49,26 @@ module.exports = function(context, req) {
     context.log("Is {}}?: " + validator("{}")); // true
     context.log("Is req.body valid?: " + validator(req.body)); // true
 
-    context.log('Body: ' + req.body);
-    context.log('channel: ' + req.body.channel);
+    // in some cases req.body comes as a valid object, sometimes as a string
+    var payload;
+    if (validator(req.body)) {
+      payload = JSON.parse(req.body);
+    } else {
+      payload = req.body;
+    }
+
+    context.log('Body: ' + payload);
+    context.log('channel: ' + payload.channel);
 
     // Build the slack API call
     var msg = {
-        channel: req.body.channel,
-        username: req.body.username,
-        icon_emoji: req.body.icon_emoji,
+        channel: payload.channel,
+        username: payload.username,
+        icon_emoji: payload.icon_emoji,
         mrkdwn: true,
-        text: req.body.text,
-        fallback: req.body.fallback,
-        attachments: noticeToAttach(req.body.notifications)
+        text: payload.text,
+        fallback: payload.fallback,
+        attachments: noticeToAttach(payload.notifications)
     };
 
     // and POST it
