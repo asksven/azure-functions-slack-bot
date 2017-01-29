2df317a454d8b52d52956f22d3c4875a2ce7b7d0
asksven <sven.knispel@gmail.com>
2017-01-29 13:55:23 +0100

    Added new function for handling AppInsights Alerts

---
+++ SlackBotAppInsightsAlerts/index.js
@@ -0,0 +1,35 @@
+module.exports = function (context, data) {
+    context.log('Webhook was triggered!');
+
+    var request = require('request');
+
+    if(data) {
+        var slackUrl = process.env["SLACK_URL"];
+        var text = {
+            "text": "Something happened!!\n<" + data.context.portalLink + "|Check it out>"
+        };
+
+        var requestData = {
+            url: slackUrl,
+            method: "POST",
+            json: true,
+            headers: {
+                "content-type": "application/json",
+            },
+            body: text
+        };
+
+        request(requestData);
+        context.res = {
+            status: 200
+        };
+    }
+    else {
+        context.res = {
+            status: 400,
+            body: { error: 'Please pass first/last properties in the input object'}
+        };
+    }
+
+    context.done();
+}
