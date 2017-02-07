7f33ecbe3758b2f82853c0f352f2495e59d3e276
asksven <sven.knispel@gmail.com>
2017-02-07 20:15:01 +0100

    Added error handling

--- SlackBotAppInsightsAlerts/index.js
+++ SlackBotAppInsightsAlerts/index.js
@@ -1,53 +1,53 @@
 module.exports = function (context, data) {
-    context.log('Function was triggered with data:' + JSON.stringify(data, null, 2));
+  context.log('Function was triggered with data:' + JSON.stringify(data, null, 2));
 
-    var request = require('request');
+  var request = require('request');
 
-    var status = 'unknown'; // will be "Activated" or "Resolved"
-    var resourceName = 'unknown';
+  var status = 'unknown'; // will be "Activated" or "Resolved"
+  var resourceName = 'unknown';
+  var text;
 
-    if(data) {
-        var slackUrl = process.env["SLACK_URL"];
-        context.log('Slack webhook: ' + slackUrl);
 
-      // we expect a message of the form described here: https://docs.microsoft.com/en-us/azure/monitoring-and-diagnostics/insights-webhooks-alerts
+  if (data) {
+    var slackUrl = process.env["SLACK_URL"];
+    context.log('Slack webhook: ' + slackUrl);
 
-        if (data.status && data.context) {
-          status = data.status;
+    // we expect a message of the form described here: https://docs.microsoft.com/en-us/azure/monitoring-and-diagnostics/insights-webhooks-alerts
 
-          if (data.context.resourceName) {
-            resourceName = data.context.resourceName;
-          }
+    try {
 
-        } else {
-          context.log('data is supposed to contain a "status" and a "context"');
-        }
+      text = {
+        "text": "Alert! " + data.context.conditionType + ": " + data.status + "\n" + data.context.name + ': ' +  + "\n" + data.context.description + "\n<" + data.context.portalLink + "|Link>"
+      };
+    } catch (e) {
+      text = {
+        "text": "An error occured. Payload was: ```" + JSON.stringify(data, null, 2) + "```"
+      };
 
-        var text = {
-            "text": "Alert " + data.context.name + ': ' + status + "\n" + data.context.description + "\n<" + data.context.portalLink + "|Link>"
-        };
+    } finally {
 
-        var requestData = {
-            url: slackUrl,
-            method: "POST",
-            json: true,
-            headers: {
-                "content-type": "application/json",
-            },
-            body: text
-        };
-
-        request(requestData);
-        context.res = {
-            status: 200
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: { error: 'Please pass first/last properties in the input object'}
-        };
     }
 
-    context.done();
+    var requestData = {
+      url: slackUrl,
+      method: "POST",
+      json: true,
+      headers: {
+          "content-type": "application/json",
+      },
+      body: text
+    };
+
+    request(requestData);
+    context.res = {
+        status: 200
+    };
+  } else {
+      context.res = {
+          status: 400,
+          body: { error: 'Please pass first/last properties in the input object'}
+      };
+  }
+
+  context.done();
 }
